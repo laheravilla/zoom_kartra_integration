3a6626c5e7f79c78471bb50b22421e24abd678e8
laheravilla <46608596+laheravilla@users.noreply.github.com>
2021-06-03 22:52:29 +0200

    Update meetting_styles_generator.js

--- meetting_styles_generator.js
+++ meetting_styles_generator.js
@@ -21,26 +21,3 @@ function insertLinkIntoHead(element) {
 links.forEach(function (value) {
             insertLinkIntoHead(createLinkElement(value));
 });
-
-
-/* Function to add style element */
-function addStyle(styles) {
-  /* Create style document */
-  var css = document.createElement('style');
-  css.type = 'text/css';
-  css.id = "yl-agency-style";
-
-  if (css.styleSheet) 
-      css.styleSheet.cssText = styles;
-  else 
-      css.appendChild(document.createTextNode(styles));
-
-  /* Append style to the tag name */
-  document.getElementsByTagName("head")[0].appendChild(css);
-}
-
-/* Set the style */
-var styles = '.js-notification-bar {display: none!important}';
-
-/* Function call */
-window.onload = function() { addStyle(styles) }()
