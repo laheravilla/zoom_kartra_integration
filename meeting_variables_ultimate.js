a29ad52599762c40fd475ca07892a8b8d709c3a6
laheravilla <46608596+laheravilla@users.noreply.github.com>
2021-06-16 05:45:17 +0200

    Create meeting_variables_ultimate.js

---
+++ meeting_variables_ultimate.js
@@ -0,0 +1,20 @@
+<!--To be placed on Meeting Page's body-->
+
+const OWNER_ID = "MISSING VALUE";
+const CUSTOM_INTERVAL = 10;
+const PAGE_TRACKING_IDS = {
+       "presentDuringFirstPart": "MISSING VALUE",
+       "presentDuringSecondPart": "MISSING VALUE",
+       "presentDuringThirdPart": "MISSING VALUE",
+       "presentDuringFourthPart": "MISSING VALUE",
+       "presentDuringFifthPart": "MISSING VALUE",
+       "presentDuringSixthPart": "MISSING VALUE",
+       "presentDuringSeventhPart": "MISSING VALUE",
+       "presentDuringEighthPart": "MISSING VALUE",
+       "presentDuringNinthParte": "MISSING VALUE",
+       "presentDuringTenthPart": "MISSING VALUE",
+       "enteredConferenceRoom": "MISSING VALUE",
+       "offerShowsUp": "MISSING VALUE",
+       "interactedWithCTAOffer": "MISSING VALUE"
+};
+const OFFER_DELAY = 0;
